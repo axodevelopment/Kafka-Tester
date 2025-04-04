c3326e976c81d9cd2f063fd7ca234b0164b4d323
Michael Wilson <michaelwilson@mikew-dev.local>
2025-04-04 02:53:14 -0400

    i think i have the producer connecting with skip verify

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Kafka-Tester
 Kafka Tester
+
+## Getting kafkauser info ready for go app
+*NOTE if you are in java you need to build a trust store instead, the secret will default to a .p12 format
+
+
+For a Go app we will just need the following:
+
+- user.crt
+- user.key
+- ca.crt
+
+I created a user called `test-kafka-user` and the broker is in `kafka-tutorial-kraft-east`
+
+```bash
+oc get secret test-kafka-user -n kafka-tutorial-kraft-east -o jsonpath='{.data.ca\.crt}' | base64 -d > ca.crt
+oc get secret test-kafka-user -n kafka-tutorial-kraft-east -o jsonpath='{.data.user\.crt}' | base64 -d > user.crt
+oc get secret test-kafka-user -n kafka-tutorial-kraft-east -o jsonpath='{.data.user\.key}' | base64 -d > user.key
+```
+
+we should be able to test the connectivity before running the app
+
+```bash
+
+openssl s_client -connect my-cluster-kraft-kafka-bootstrap-kafka-tutorial-kraft-east.apps.axolab.axodevelopment.dev:443 -cert user.crt -key user.key -CAfile ca.crt -verify_return_error
+```
--- main.go
+++ main.go
@@ -1,19 +1,34 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"strings"
 	"syscall"
 	"time"
+
+	"github.com/IBM/sarama"
 )
 
 const (
 	SERVICE_NAME = "KAFKA-TESTER"
 )
 
-func log(args ...string) {
+var (
+	bootstrapServer = ""
+	topic           = ""
+	certFile        = ""
+	keyFile         = ""
+	caFile          = ""
+	tlsSkipVerify   = true
+	useTLS          = true
+)
+
+func logd(args ...string) {
 	var builder strings.Builder
 
 	for i := range args {
@@ -22,7 +37,6 @@ func log(args ...string) {
 		if i < (len(args) - 1) {
 			builder.WriteString(" - ")
 		}
-
 	}
 
 	stime := time.Now().Format(time.UnixDate)
@@ -31,12 +45,69 @@ func log(args ...string) {
 }
 
 func main() {
-	defer log("Application Exiting ...")
-	log("Application String ...")
+	defer logd("Application Exiting ...")
+	logd("Application String ...")
+
+	bootstrapServer = "my-cluster-kraft-kafka-bootstrap-kafka-tutorial-kraft-east.apps.axolab.axodevelopment.dev:443"
+	certFile = "user.crt"
+	keyFile = "user.key"
+	caFile = "ca.crt"
+	tlsSkipVerify = true
+	useTLS = true
 
 	osSig := make(chan os.Signal, 2)
 
 	signal.Notify(osSig, os.Interrupt, syscall.SIGINT, syscall.SIGTERM, syscall.SIGPIPE)
 
+	config := sarama.NewConfig()
+	config.Version = sarama.V3_9_0_0
+
+	if useTLS {
+		config.Net.TLS.Enable = true
+		config.Net.TLS.Config = createTLSConfiguration()
+	}
+
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = 5
+	config.Producer.Return.Successes = true
+
+	producer, err := sarama.NewSyncProducer([]string{bootstrapServer}, config)
+
+	if err != nil {
+		log.Fatal("Failed to create producer: %v", err)
+
+	}
+
+	defer producer.Close()
+
 	<-osSig
+}
+
+func createTLSConfiguration() (t *tls.Config) {
+	t = &tls.Config{
+		InsecureSkipVerify: tlsSkipVerify,
+	}
+
+	if certFile != "" && keyFile != "" && caFile != "" {
+		cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		caCert, err := os.ReadFile(caFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		caCertPool := x509.NewCertPool()
+		caCertPool.AppendCertsFromPEM(caCert)
+
+		t = &tls.Config{
+			Certificates:       []tls.Certificate{cert},
+			RootCAs:            caCertPool,
+			InsecureSkipVerify: true,
+		}
+	}
+
+	return t
 }
